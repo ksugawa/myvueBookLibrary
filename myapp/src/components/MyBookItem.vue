75aa600e525976fb42dbe5c1b75b8fd4c6adf570
k-sugawa <sugawakumiko@gmail.com>
2023-12-22 20:14:23 +0900

    個々の本の情報

---
+++ myapp/src/components/MyBookItem.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { props } = defineProps(['title', 'author', 'review'])
+</script>
+
+<template>
+  <li>
+    <img src="" alt="">
+    <p>{{ title }}</p>
+    <p>{{ author }}</p>
+    <div></div>
+    <p>{{ review }}</p>
+  </li>
+</template>
+
+<style scoped></style>
