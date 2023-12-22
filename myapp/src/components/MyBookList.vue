c9e21b2a4839d38332e2398d7e2ea55212575c64
k-sugawa <sugawakumiko@gmail.com>
2023-12-22 20:14:42 +0900

    本おリスト作成

---
+++ myapp/src/components/MyBookList.vue
@@ -0,0 +1,34 @@
+<script setup>
+import MyBookItem from './MyBookItem.vue';
+</script>
+
+<template v-if="active">
+        <ul class="book-list">
+            <MyBookItem />
+        </ul>
+</template>
+
+<style scoped>
+.book-list {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.book-item {
+    width: 14%;
+}
+
+.book-img {
+    height: 255px;
+    background-color: #ccc;
+}
+
+.author {
+    font-size: 9px;
+    color: #7d7d7d;
+}
+
+</style>
