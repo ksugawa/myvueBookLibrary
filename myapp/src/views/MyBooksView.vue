92c43ce96a8f3375c6043eef8b5359fe6f08c52f
k-sugawa <sugawakumiko@gmail.com>
2024-01-02 22:52:05 +0900

    すべての本を表示するページを追加

---
+++ myapp/src/views/MyBooksView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MyBookList from "../components/MyBookList.vue";
+import SearchMyBook from "@/components/SearchMyBook.vue";
+</script>
+
+<template>
+  <SearchMyBook />
+  <MyBookList />
+</template>
+
+<style scoped>
+body {
+  background-color: #f5f5f5;
+}
+</style>
