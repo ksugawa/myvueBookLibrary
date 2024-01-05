bc236301ebf2849949af532d203344eeb7d5b5d3
k-sugawa <sugawakumiko@gmail.com>
2024-01-05 17:30:15 +0900

    変更

--- myapp/src/router/index.js
+++ myapp/src/router/index.js
@@ -1,28 +1,28 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/mybooks',
-        name: 'mybooks',
-        component: () => import('../views/MyBooksView.vue')
-    },
-    {
-        path: '/mybooks/:id',
-        name: 'mybookpage',
-        component: () => import('../views/MyBookPageView.vue')
-
-    }
-]
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/mybooks",
+    name: "mybooks",
+    component: () => import("../views/MyBooksView.vue"),
+  },
+  {
+    path: "/mybooks/:id",
+    name: "mybookpage",
+    props: true,
+    component: () => import("../views/MyBookPageView.vue"),
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-  })
-  
-  export default router
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
